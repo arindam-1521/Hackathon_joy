0111fb3be6d3649239159ec19c1b6c1e340a0405
arindam <arindampradhan562@gmail.com>
2023-03-04 21:57:02 +0530

    padding added

--- ques3.js
+++ ques3.js
@@ -31,7 +31,7 @@ let url = "https://kontests.net/api/v1/all"
 const addFunc = async () => {
     const response = await fetch(url)
     const contests = await response.json()
-    console.log(contests)
+    // console.log(contests)
     var ihtml = ""
     var arr = ["https://lumen5.com/learn/wp-content/uploads/2019/07/The-6-Best-Contest-Software.jpg", "https://i.pinimg.com/originals/58/8b/2a/588b2a7cc8feda90fbeba669fdcc242e.jpg", "https://i.pinimg.com/736x/ab/ae/64/abae64e5e226ef5f503ce38d140e3ee4.jpg"]
     for (item in contests) {
@@ -39,7 +39,7 @@ const addFunc = async () => {
         let n = Math.floor(i)
         // console.log(contests[item])
         ihtml += `
-        <div class="card mx-2 my-2" style="width: 20rem;border:0px;box-shadow:0px 0px 15px #878787;border-radius:16px">
+        <div class="card mx-2 my-2 p-1" style="width: 20rem;border:0px;box-shadow:0px 0px 15px #878787;border-radius:16px;">
         <img style="border-radius:16px" src= ${arr[n]} class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${contests[item].name}</h5>
